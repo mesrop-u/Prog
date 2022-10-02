55c2aca89ec674df826ff5e674754bc908d8a853
mesrop-u <67364207+mesrop-u@users.noreply.github.com>
2022-10-02 14:46:00 +0400

    Add files via upload

---
+++ prog3EndWork/server.js
@@ -0,0 +1,248 @@
+var express = require('express');
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
+
+
+
+app.use(express.static("."));
+
+app.get('/', function (req, res) {
+   res.redirect('index.html');
+});
+
+server.listen(3000),() =>{
+console.log("run")
+};
+
+function generator(matLen, gr, gre, pr, snake, poise) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < gre; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+    for (let i = 0; i < pr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < snake; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < poise; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    return matrix;
+}
+let matrix = generator(20, 10, 5, 2, 2, 3);
+
+io.sockets.emit('send matrix',matrix)
+
+grassArr = []
+GrassEaterArr = []
+PredatorArr = []
+SnakeArr = []
+PoiseArr = []
+
+
+
+Grass1 = require("./grass")
+GrassEater1 = require("./grasseater")
+Predator1 = require("./predator")
+Snake1 = require("./snake")
+Poise1 = require("./poise")
+weath = "winter";
+
+function createObject(matrix){
+   for (var y = 0; y < matrix.length; y++) {
+       for (var x = 0; x < matrix[y].length; x++) {
+           if (matrix[y][x] == 1) {
+               let gr = new Grass(x, y)
+               grassArr.push(gr)
+           } else if (matrix[y][x] == 2) {
+               let gre = new GrassEater(x, y)
+               GrassEaterArr.push(gre)
+           } else if (matrix[y][x] == 3) {
+               let pr = new Predator(x, y)
+               PredatorArr.push(pr)
+           } else if (matrix[y][x] == 4) {
+               let snake = new Snake(x, y)
+               PoiseArr.push(hum)
+           } else if (matrix[y][x] == 5) {
+               let poise = new Poise(x, y)
+               PoiseArr.push(gar)
+           }
+       }
+   }
+   io.sockets.emit('send matrix',matrix)
+   
+}
+
+function game(){
+   for (let i in grassArr) {
+      grassArr[i].mul()
+  }
+
+
+  for (let i in GrassEaterArr) {
+      GrassEaterArr[i].mul()
+      GrassEaterArr[i].eat()
+  }
+  for (let i in PredatorArr) {
+      PredatorArr[i].mul()
+      PredatorArr[i].eat()
+  }
+  for (let i in SnakeArr) {
+      SnakeArr[i].mul()
+      SnakeArr[i].move()
+  }
+  for (let i in PoiseArr) {
+      PoiseArr[i].mul()
+      PoiseArr[i].move()
+  }
+  io.sockets.emit('send matrix',matrix)
+}
+setInterval(game,200)
+
+function kill() {
+   grassArr = [];
+   grassEaterArr = []
+   for (var y = 0; y < matrix.length; y++) {
+       for (var x = 0; x < matrix[y].length; x++) {
+           matrix[y][x] = 0;
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function addGrass() {
+   for (var i = 0; i < 5; i++) {
+   var x = Math.floor(Math.random() * matrix[0].length)
+   var y = Math.floor(Math.random() * matrix.length)
+       if (matrix[y][x] == 0) {
+           matrix[y][x] = 1
+           var gr = new Grass(x, y, 1)
+           grassArr.push(gr)
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function addGrassEater() {
+   for (var i = 0; i < 3; i++) {
+   var x = Math.floor(Math.random() * matrix[0].length)
+   var y = Math.floor(Math.random() * matrix.length)
+       if (matrix[y][x] == 0) {
+           matrix[y][x] = 2
+           var gre = new GrassEater(x, y, 2)
+           GrassEaterArr.push(gre)
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function addPredator() {
+   for (var i = 0; i < 5; i++) {
+   var x = Math.floor(Math.random() * matrix[0].length)
+   var y = Math.floor(Math.random() * matrix.length)
+       if (matrix[y][x] == 0) {
+           matrix[y][x] = 3
+           var pr = new Predator(x, y, 3)
+           predatorArr.push(pr)
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function addSnake() {
+   for (var i = 0; i < 5; i++) {
+   var x = Math.floor(Math.random() * matrix[0].length)
+   var y = Math.floor(Math.random() * matrix.length)
+       if (matrix[y][x] == 0) {
+           matrix[y][x] = 4
+           var sn = new Snake(x, y, 4)
+           SnakeArr.push(sn)
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function addPoise() {
+   for (var i = 0; i < 3; i++) {
+   var x = Math.floor(Math.random() * matrix[0].length)
+   var y = Math.floor(Math.random() * matrix.length)
+       if (matrix[y][x] == 0) {
+           matrix[y][x] = 5
+           var poise = new Poise(x, y, 5)
+           PoiseArr.push(poise)
+       }
+   }
+   io.sockets.emit("send matrix", matrix);
+}
+
+function weather() {
+   if (weath == "winter") {
+       weath = "spring"
+   }
+   else if (weath == "spring") {
+       weath = "summer"
+   }
+   else if (weath == "summer") {
+       weath = "autumn"
+   }
+   else if (weath == "autumn") {
+       weath = "winter"
+   }
+   io.sockets.emit('weather', weath)
+}
+setInterval(weather, 5000);
+
+io.on('connection', function(){
+   createObject(matrix)
+   socket.on("kill", kill);
+   socket.on("add grass", addGrass);
+   socket.on("add grassEater", addGrassEater);
+   socket.on("add predator", addPredator);
+   socket.on("add snake", addSnake);
+   socket.on("add poise", addPoise);
+})
+var statistics = {}
+setInterval(function() {
+   statistics.grass = grassArr.length;
+   statistics.grassEater = GrassEaterArr.length;
+   statistics.predator = PredatorArr.length;
+   statistics.snake = SnakeArr.length;
+   statistics.poise = PoiseArr.length;
+   fs.writeFile("statistics.json", JSON.stringify(statistics), function(){
+       console.log("send")
+   })
+},1000)
+
---
+++ prog3EndWork/snake.js
@@ -0,0 +1,109 @@
+let LivingCreature = require("./LivingCreature")
+
+module.exports=class Snake extends LivingCreature{
+    
+    mul() {
+        this.multiply++
+        var emptyCells = super.chooseCell(0);
+        var newCell = emptyCells(Math.floor(Math.random() * emptyCells.length));
+        if (newCell && this.multiply >= 15) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 4;
+
+            var newSnake = new Snake(newX, newY);
+            SnakeArr.push(newSnake);
+            this.multiply = 0;
+        }
+    }
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        var emptyCells1 = this.chooseCell(1)
+        var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
+
+        if (newCell && this.energy >= 0) {
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+        if (newCell1 && this.energy >= 0) {
+            let newX = newCell1[0]
+            let newY = newCell1[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 1
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+    eat() {
+        var emptyCells = this.chooseCell(2)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        var emptyCells1 = this.chooseCell(5)
+        var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells.length)]
+
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in GrassEaterArr) {
+                if (newX == GrassEaterArr[i].x && newY == GrassEaterArr[i].y) {
+                    GrassEaterArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+        if (newCell1) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in PoiseArr) {
+                if (newX == PoiseArr[i].x && newY == PoiseArr[i].y) {
+                    PoiseArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+        
+      
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in SnakeArr) {
+            if (this.x == SnakeArr[i].x && this.y == SnakeArr[i].y) {
+                SnakeArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
